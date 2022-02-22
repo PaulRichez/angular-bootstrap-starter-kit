31a88723b2b4a1415263d19917bcd28fa22128fa
PaulRichez <paul.richez59@gmail.com>
2022-02-22 23:45:42 +0100

    theme switch

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/theme.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private theme: ThemeService) { }
 
   ngOnInit(): void {
   }
-
+  public switchTheme(): void {
+    if (this.theme.current === 'light') {
+      this.theme.current = 'united';
+    } else {
+      this.theme.current = 'light';
+    }
+  }
 }
