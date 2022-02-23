6d374cf392f97a0cb38375bcf8e2439b02c36683
PaulRichez <paul.richez59@gmail.com>
2022-02-23 01:26:49 +0100

    ngx bootstrap

--- src/app/pages/ngx-bootstrap-page/ngx-bootstrap-page.component.ts
+++ src/app/pages/ngx-bootstrap-page/ngx-bootstrap-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-ngx-bootstrap-page',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ngx-bootstrap-page.component.scss']
 })
 export class NgxBootstrapPageComponent implements OnInit {
-
-  constructor() { }
+  modalRef?: BsModalRef;
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit(): void {
   }
-
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 }
--- src/app/pages/ngx-bootstrap-page/ngx-bootstrap-page.module.ts
+++ src/app/pages/ngx-bootstrap-page/ngx-bootstrap-page.module.ts
@@ -4,12 +4,28 @@ import { CommonModule } from '@angular/common';
 import { NgxBootstrapPageRoutingModule } from './ngx-bootstrap-page-routing.module';
 import { NgxBootstrapPageComponent } from './ngx-bootstrap-page.component';
 
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { PopoverModule } from 'ngx-bootstrap/popover';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [NgxBootstrapPageComponent],
   imports: [
     CommonModule,
-    NgxBootstrapPageRoutingModule
+    NgxBootstrapPageRoutingModule,
+    AccordionModule,
+    AlertModule,
+    TabsModule,
+    PaginationModule,
+    ProgressbarModule,
+    TooltipModule,
+    PopoverModule,
+    ModalModule.forRoot(),
   ]
 })
 export class NgxBootstrapPageModule { }
